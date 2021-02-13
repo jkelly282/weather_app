df109a45519efea3d56fd627912f12f23964840b
jkelly2 <jkelly2@illumina.com>
2021-02-13 16:47:12 +0000

    added mapping functionality

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
+import base64
 import json
 import sys
+from flask_table import Table, Col
 
+import geopandas
+import matplotlib
 from werkzeug.utils import redirect
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 import requests
 from flask_wtf import FlaskForm
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, make_response
 from dotenv import load_dotenv
 from config import Config
 from constants import BASE_URL, FORBIDDEN_CITY
+from shapely.geometry import Point
+import geopandas as gpd
+from geopandas import GeoDataFrame
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')
+from pylab import rcParams
+import io
+import os
+import glob
+
 load_dotenv()
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -24,7 +39,7 @@ def return_weather_info(city:str) -> json:
     :param city: Name of the city to return weather information
     :return: Weather information of the cty in json format
     '''
-    url = f'{BASE_URL}{city}&appid={Config.SECRET_KEY}'
+    url = f'{BASE_URL}{city}&appid={Config.SECRET_KEY}&units=metric'
     response = requests.get(url)
     try:
         response.raise_for_status()
@@ -41,7 +56,7 @@ def index():
     if form.validate_on_submit():
         weather_info = return_weather_info(form.city.data)
         if form.city.data.upper() == FORBIDDEN_CITY.upper():
-            sys.exit()
+            return redirect(url_for('forbidden'))
         return redirect(url_for('weather_display', weather_info = weather_info))
 
     return render_template('home.html', title = 'Home', form=form)
@@ -49,4 +64,56 @@ def index():
 
 @app.route('/weather/<weather_info>')
 def weather_display(weather_info:json):
-  return render_template('weather.html', weather_info =weather_info)
+    weather_info = weather_info.replace("\'", "\"")
+    weather_info = json.loads(weather_info)
+    image = generate_image(weather_info.get('coord').get('lon'), weather_info.get('coord').get('lat'))
+    resp = make_response(render_template('weather.html', weather_info = weather_info, user_image = image))
+    return resp
+
+def generate_image(longitude, latidude):
+    delete_files('./static/map/')
+    d = {'col1': ['name1'], 'geometry': [Point(longitude, latidude)]}
+    gdf = gpd.GeoDataFrame(d, crs="EPSG:4326")
+
+    world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+
+    # We restrict to South America.
+    ax = world.plot(
+        color='white', edgecolor='black')
+
+    # We can now plot our ``GeoDataFrame``.
+    gdf.plot(ax=ax, color='red')
+
+    plt.savefig(f'./static/map/{longitude}_{latidude}.png')
+    return f'/static/map/{longitude}_{latidude}.png'
+
+
+
+@app.route('/forbidden')
+def forbidden():
+
+    return render_template('forbidden_city.html')
+
+
+def delete_files(final_directory: str):
+    """
+    Deletes all files within a specified directory
+
+    :param: final directory: The directory to remove the files from
+    """
+    if os.path.exists(final_directory):
+        files = glob.glob(f"{final_directory}/*")
+        for f in files:
+            try:
+                os.remove(f)
+            except FileNotFoundError:
+                logging.warning(f"File: {f} could not be found")
+        if len(os.listdir(final_directory)) > 0:
+            logging.error(f"The directory {final_directory} was not empty - exiting")
+            sys.exit()
+    else:
+        logging.error(f"The directory {final_directory} does not exist - exiting")
+        raise AssertionError
+
+
+
